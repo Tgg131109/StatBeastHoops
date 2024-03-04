5acc84824eeff819421bc168e5fd377278cd7790
Tgg131109 <tobyg7687@gmail.com>
2024-03-04 15:06:02 -0500

    initial commit

---
+++ StatBeastHoops/StatBeastHoops/StatBeastHoopsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatBeastHoopsApp.swift
+//  StatBeastHoops
+//
+//  Created by Toby Gamble on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct StatBeastHoopsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
